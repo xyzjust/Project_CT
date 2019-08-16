ce1f58e56ec6f2ffc0801313fd33d13d286ca9b9
windows <junyi_429730731@hotmail.co.uk>
2019-08-17 00:05:53 +0100

    new item

---
+++ Code_Base/Quick_Plots.py
@@ -0,0 +1,1 @@
+from ../essential_imports import *
---
+++ Code_Base/essential_imports.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas_profiling
+import warnings
+import datetime as dt
+from datetime import datetime
+
+print("""
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import pandas_profiling
+import warnings
+""")
